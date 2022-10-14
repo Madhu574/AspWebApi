7314893167df4ce3dec39b7d2f07ff84eb4e9564
imtiajahammad <imtiajahammad.official@gmail.com>
2022-10-14 12:23:24 +0600

    Repository interfaces added

---
+++ IRepositories/IDepartmentRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using EmployeeMCrud.Models;
+
+namespace EmployeeMCrud.IRepositories
+{
+    public interface IDepartmentRepository
+    {
+        Task<IEnumerable<Department>> GetDepartment();
+        Task<Department> GetDepartmentByID(int ID);
+        Task<Department> InsertDepartment(Department objDepartment);
+        Task<Department> UpdateDepartment(Department objDepartment);
+        bool DeleteDepartment(int ID);
+    }
+}
+
---
+++ IRepositories/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using EmployeeMCrud.Models;
+
+namespace EmployeeMCrud.IRepositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetEmployees();
+        Task<Employee> GetEmployeeByID(int ID);
+        Task<Employee> InsertEmployee(Employee objEmployee);
+        Task<Employee> UpdateEmployee(Employee objEmployee);
+        bool DeleteEmployee(int ID);
+    }
+}
+
