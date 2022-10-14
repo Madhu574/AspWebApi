14c1c8b4f1ef850b38950e456c0998cc4a968612
imtiajahammad <imtiajahammad.official@gmail.com>
2022-10-14 12:24:42 +0600

    configurations added

--- Program.cs
+++ Program.cs
@@ -1,26 +1,94 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using EmployeeMCrud.IRepositories;
+using EmployeeMCrud.Models;
+using EmployeeMCrud.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddDbContext<APIDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("EmployeeAppCon")));
+builder.Services.AddSwaggerGen(options => {
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "WEB API",
+        Version = "v1"
+    });
+});
+//Enable CORS
+builder.Services.AddCors(c => {
+    c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
+//JSON Serializer
+builder.Services.AddControllersWithViews().AddNewtonsoftJson(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore).AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+
 }
+app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseRouting();
 
-app.MapControllers();
+app.UseAuthorization();
+app.UseEndpoints(endpoints => {
+    endpoints.MapControllers();
+});
+//app.MapControllers();
+app.UseSwagger();
+app.UseSwaggerUI(c => {
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "WEB API");
+    c.DocumentTitle = "WEB API";
+    c.DocExpansion(DocExpansion.List);
+});
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Photos")),
+    RequestPath = "/Photos"
+});
 
 app.Run();
 
+
+
+
+
+
+/*
+
+source: https://www.c-sharpcorner.com/article/net-core-3-1-web-api-crud/
+
+commands:
+        dotnet ef migrations add MyFirstMigrations=
+        dotnet ef database update
+        dotnet ef migrations remove
+        dotnet ef database update MyFirstMigrations
+        dotnet ef migrations script
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
