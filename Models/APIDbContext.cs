750fe8e55dfc0223285eaed5ab0fbff58610069f
imtiajahammad <imtiajahammad.official@gmail.com>
2022-10-14 12:23:01 +0600

    models added including dbcontext model

---
+++ Models/APIDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmployeeMCrud.Models
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions<APIDbContext> options) : base(options) { }
+        public DbSet<Department> Departments
+        {
+            get;
+            set;
+        }
+        public DbSet<Employee> Employees
+        {
+            get;
+            set;
+        }
+    }
+}
+
---
+++ Models/Department.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EmployeeMCrud.Models
+{
+    [Table("Department")]
+    public class Department
+    {
+        [Key]
+        public int DepartmentId { get; set; }
+
+        public string DepartmentName { get; set; }
+    }
+}
+
