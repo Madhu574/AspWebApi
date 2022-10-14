1f21b94f320b5b303dd7d67bb8918051b9454196
imtiajahammad <imtiajahammad.official@gmail.com>
2022-10-14 12:24:58 +0600

    controllers added

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeMCrud.IRepositories;
+using EmployeeMCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace EmployeeMCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employee;
+        private readonly IDepartmentRepository _department;
+        private IHostingEnvironment _environment;
+        public EmployeeController(IEmployeeRepository employee, IDepartmentRepository department, IHostingEnvironment environment)
+        {
+            _employee = employee ??
+                throw new ArgumentNullException(nameof(employee));
+            _department = department ??
+                throw new ArgumentNullException(nameof(department));
+            _environment = environment;
+        }
+        [HttpGet]
+        [Route("GetEmployee")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _employee.GetEmployees());
+        }
+        [HttpGet]
+        [Route("GetEmployeeByID/{Id}")]
+        public async Task<IActionResult> GetEmpByID(int Id)
+        {
+            return Ok(await _employee.GetEmployeeByID(Id));
+        }
+        [HttpPost]
+        [Route("AddEmployee")]
+        public async Task<IActionResult> Post(Employee emp)
+        {
+            var result = await _employee.InsertEmployee(emp);
+            if (result.EmployeeID == 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Something Went Wrong");
+            }
+            return Ok("Added Successfully");
+        }
+        [HttpPut]
+        [Route("UpdateEmployee")]
+        public async Task<IActionResult> Put(Employee emp)
+        {
+            await _employee.UpdateEmployee(emp);
+            return Ok("Updated Successfully");
+        }
+        [HttpDelete]
+        [Route("DeleteEmployee")]
+        //[HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            var result = _employee.DeleteEmployee(id);
+            return new JsonResult("Deleted Successfully");
+        }
+
+        [Route("SaveFile")]
+        [HttpPost]
+        public JsonResult SaveFile()
+        {
+            try
+            {
+                var httpRequest = Request.Form;
+                var postedFile = httpRequest.Files[0];
+                string filename = postedFile.FileName;
+                var physicalPath = _environment.ContentRootPath + "/Photos/" + filename;
+
+                using (var stream = new FileStream(physicalPath, FileMode.Create))
+                {
+                    stream.CopyTo(stream);
+                }
+
+                return new JsonResult(filename);
+            }
+            catch (Exception)
+            {
+                return new JsonResult("anonymous.png");
+            }
+        }
+
+        [HttpGet]
+        [Route("GetDepartment")]
+        public async Task<IActionResult> GetAllDepartmentNames()
+        {
+            return Ok(await _department.GetDepartment());
+        }
+    }
+}
+
